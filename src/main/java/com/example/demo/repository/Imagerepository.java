54430fe5c17869337d2077d73bd35e888680c429
kubo <y.kubo0729@gmail.com>
2025-05-07 19:51:34 +0900

    html was amended.

---
+++ src/main/java/com/example/demo/repository/Imagerepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+/*
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.dto.ImageForm;
+
+public interface Imagerepository extends JpaRepository<ImageForm, Integer> {}
+
+*/
